ca5996b282a8dab657b04e8d2a7e60662f0d2c19
BrightBtw <diulsya96@gmail.com>
2026-08-10 01:30:17 +0500

    fix quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
 import {createElement} from "react"
 export default function App() {
-  return createElement('h3', {className:'quote'}, 'May the Force be with you')
+  return createElement('h3', {className:'quote'}, 'May the Force be with you \u00A9')
 }
